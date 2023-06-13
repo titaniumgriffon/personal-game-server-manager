fd075f6a7d5e62100c4af57f1cde826f2961b1cc
Luke Mason <titaniumgriffon@users.noreply.github.com>
2023-06-12 21:05:06 -0500

    Update 7dtd.sh

--- Bash/7dtd.sh
+++ Bash/7dtd.sh
@@ -7,8 +7,6 @@ sudo apt -y install apt-transport-https ca-certificates curl gnupg-agent softwar
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
-sudo apt install docker-ce docker-ce-cli containerd.io
-sudo apt update
 
 #install AWS CLI
 sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
